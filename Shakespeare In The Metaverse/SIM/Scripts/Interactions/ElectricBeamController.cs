7adb5e6225079b111e0d81dec6168113191c131f
MylesXR <119466865+MylesXR@users.noreply.github.com>
2023-11-23 21:18:01 -0700

    commenting vicinity and electric beam controller

--- Shakespeare In The Metaverse/SIM/Scripts/Interactions/ElectricBeamController.cs
+++ Shakespeare In The Metaverse/SIM/Scripts/Interactions/ElectricBeamController.cs
@@ -7,32 +7,39 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ElectricBeamController : NetworkBehaviour
 {
-    public ParticleSystem particleEffect;
-    public InputActionReference inputActionReference;
-    public AudioSource audioSource;
-    public Color defaultParticleColor;
-    public Color interactionPointColor;
+    public ParticleSystem particleEffect; // Particle system for the electric beam effect
+    public InputActionReference inputActionReference; // input action for triggering the electric beam
+    public AudioSource audioSource; // AudioSource for playing sound effects
+    public Color defaultParticleColor; // Default color of the particle effect
+    public Color interactionPointColor; // Color of the particle effect when pointing at an interaction point
 
-    // Cooldown time in seconds, adjustable from the Unity inspector
+    // Cooldown time in seconds, adjustable from the Unity inspector, used to prevent spamming
     public float cooldownTime = 1.0f;
 
-    private bool isPlayer;
-    private float lastReleaseTime;
-    private bool isCooldown;
-    private bool wasActive;
-    private bool activationRequested;
-    private ParticleSystem.Particle[] particles;
+    private bool isPlayer; // Flag to check if the controller is associated with a player
+    private float lastReleaseTime; // Time of the last button release
+    private bool isCooldown; // Flag to indicate if the electric beam is in cooldown
+    private bool wasActive; // Flag to track if the electric beam was active
+    private bool activationRequested; // Flag to indicate if activation was requested during cooldown
+    private ParticleSystem.Particle[] particles; // Array to store particle information
 
     private void Start()
     {
+        // Check if the module is Shakespeare In the Metavrse if not do not apply to controller
         if (ModuleManager.Settings.module != KernSettings.Module.SIM)
         {
             this.transform.parent.gameObject.SetActive(false);
             return;
         }
+
+        // Attach methods to input action events
         inputActionReference.action.performed += OnButtonPress;
         inputActionReference.action.canceled += OnButtonRelease;
+
+        // player is the Kern avatarcontroller
         isPlayer = GetComponentInParent<AvatarController>().Object.HasInputAuthority;
+
+        // Initialize variables
         lastReleaseTime = 0.0f;
         isCooldown = false;
         wasActive = false;
@@ -42,6 +49,7 @@ public class ElectricBeamController : NetworkBehaviour
 
     private void OnDisable()
     {
+        // Detach methods from input action events
         inputActionReference.action.performed -= OnButtonPress;
         inputActionReference.action.canceled -= OnButtonRelease;
     }
@@ -94,7 +102,7 @@ public class ElectricBeamController : NetworkBehaviour
             }
         }
 
-        var main = particleEffect.main;  // Define main here so it can be accessed later
+        var main = particleEffect.main;  
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit))
@@ -112,7 +120,7 @@ public class ElectricBeamController : NetworkBehaviour
 
             int numParticlesAlive = particleEffect.GetParticles(particles);
 
-            // Change the color of all live particles.
+            // Change the color of all live particles, used because particles were sometimes two colors when hover stopped
             for (int i = 0; i < numParticlesAlive; i++)
             {
                 particles[i].startColor = main.startColor.color;
@@ -123,6 +131,7 @@ public class ElectricBeamController : NetworkBehaviour
         }
     }
 
+    //others need to see your lightning color to know if all are activating Interaction points
     [Rpc(RpcSources.All, RpcTargets.All)]
     private void RPC_ActivateParticleEffect(bool isActive)
     {
@@ -130,11 +139,13 @@ public class ElectricBeamController : NetworkBehaviour
         {
             if (isActive)
             {
+                // Start particle system and play audio
                 particleEffect.Play();
                 audioSource.Play();
             }
             else
             {
+                // Stop particle system and audio
                 particleEffect.Stop();
                 audioSource.Stop();
             }
---
+++ Shakespeare In The Metaverse/SIM/Scripts/Interactions/VicinityTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VicinityTrigger : MonoBehaviour
+{
+    // Unity events triggered on specific actions
+    [SerializeField] UnityEvent onEnter = null; // Event triggered when an object enters the vicinity
+    [SerializeField] UnityEvent onHover = null; // Event triggered when an object is in the vicinity
+    [SerializeField] UnityEvent onExit = null; // Event triggered when an object exits the vicinity
+
+    // Timeout duration for triggering exit event, prevents spamming
+    [SerializeField] float timeout = 1;
+
+    // Flag to control whether to show the raycast line for debugging
+    [SerializeField] public bool showRaycastLine = true;
+
+    // Variables to track time and vicinity state
+    float enterTime;
+    bool inVicinity = false;
+
+
+    void Update()
+    {
+        // Check if in vicinity and timeout has passed
+        if (inVicinity && Time.time > enterTime + timeout)
+        {
+            // Object has been in vicinity for the specified time, trigger exit event
+            inVicinity = false;
+            onExit?.Invoke();
+        }
+    }
+
+    // Method called when the object is touched or enters the vicinity
+    public void Touch()
+    {
+        // Check if not already in the vicinity
+        if (!inVicinity)
+            onEnter?.Invoke(); // Trigger enter event
+
+        // Set inVicinity flag to true and update enter time
+        inVicinity = true;
+        enterTime = Time.time;
+
+        // Trigger hover event
+        onHover?.Invoke();
+    }
+}
